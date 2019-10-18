6c1c5e6e2871bc144a5b3a547efbb4c72378e125
BangTD <tdb6596@gmail.com>
2019-10-18 09:49:43 +0700

    Thuc Hanh3

---
+++ TH.js
@@ -0,0 +1,37 @@
+function Circle(x, y, radius){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+}
+
+function getRandomHex(){
+    return Math.floor(Math.random()*255);
+}
+
+function getRandomColor(){
+    var red = getRandomHex();
+    var green = getRandomHex();
+    var blue = getRandomHex();
+    return "rgb(" + red + "," + blue + "," + green +")";
+}
+
+function createCircle(){
+    var ctx = document.getElementById("myCanvas").getContext("2d");
+    var radius = Math.floor(Math.random() * 80);
+    var color = getRandomColor();
+    var x = Math.random() * window.innerWidth;
+    var y = Math.random() * window.innerHeight;
+    var circle= new Circle(x, y, radius);
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+
+function createMultipleCircle(){
+    for(var i = 0; i < 30; i++){
+        createCircle();
+    }
+}
+
+createMultipleCircle();
---
+++ demo.js
@@ -0,0 +1,3 @@
+function demo() {
+    document.getElementById("demop").innerHTML = Date();
+}
